cdafa0d068af874488e690b8f3ee0244b70ca3ec
Michał Stanisławski <michal.stanislawski098@gmail.com>
2023-03-22 09:25:02 +0100

    before lab 3

--- inc/job.hh
+++ inc/job.hh
@@ -1,27 +1,29 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <algorithm>
 
 class Job
 {
 private:
+    int _id;
     int _p; // czas trwania
     int _w; // waga
     int _d; // pożądany czas zakończenia
 
 public:
-    Job(int p, int w, int d) : _p(p), _w(w), _d(d) {}
+    Job(int id, int p, int w, int d) : _id(id), _p(p), _w(w), _d(d) {}
 
+    int getId() const { return _id; }
     int getWeight() const { return _w; }
     int getLength() const { return _p; }
-    int getDeadline() const { return _d; }    
-
-    int getDelay(int c) const
-    {
-        return c > _d ? c - _d : 0;
-    }
+    int getDeadline() const { return _d; }
+    int getDelay(int c) const { return c > _d ? c - _d : 0; }
+    int getPenalty(int c) const { return getDelay(c) * _w; }
 };
 
 int getWeightedDelaysSum(std::vector<Job> jobs);
 std::vector<Job> getJobsFromFile(std::string filename, int dataset_number);
 std::vector<Job> getPDAlgorithmJobs(std::vector<Job> jobs);
+std::vector<Job> getBruteForceMinPenaltyJobs(std::vector<Job> jobs);
+int getCmax(std::vector<Job> jobs);
--- src/job.cpp
+++ src/job.cpp
@@ -4,9 +4,17 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <queue>
 #include "../inc/job.hh"
 
-int getWeightedDelaysSum(std::vector<Job> jobs)
+/**
+ * @brief Get the Weighted Delays Sum number, known as WiTi.
+ * This is our score, this value should be minimized.
+ *
+ * @param jobs
+ * @return int
+ */
+int getWeightedDelaysSum(const std::vector<Job>& jobs)
 {
     int c = 0;
     int delays = 0;
@@ -18,8 +26,20 @@ int getWeightedDelaysSum(std::vector<Job> jobs)
     return delays;
 }
 
-std::vector<Job> getPDAlgorithmJobs(std::vector<Job> jobs)
-{    
+/**
+ * @brief Get length of all jobs - or time needed to complete all jobs.
+ *
+ * @param jobs
+ * @return int
+ */
+int getCmax(const std::vector<Job>& jobs)
+{
+    int cmax = 0;
+    for (auto job : jobs)
+    {
+        cmax += job.getLength();
+    }
+    return cmax;
 }
 
 std::vector<Job> getJobsFromFile(std::string filename, int dataset_number)
@@ -34,6 +54,7 @@ std::vector<Job> getJobsFromFile(std::string filename, int dataset_number)
     std::vector<Job> jobs;
     int p, w, d;
     bool found_dataset = false;
+    int id = 0;
 
     while (std::getline(file, line))
     {
@@ -55,7 +76,8 @@ std::vector<Job> getJobsFromFile(std::string filename, int dataset_number)
                 {
                     throw std::runtime_error("Error reading job data from file");
                 }
-                jobs.push_back(Job(p, w, d));
+                jobs.push_back(Job(id, p, w, d));
+                id++;
             }
             found_dataset = true;
             break;
@@ -68,4 +90,32 @@ std::vector<Job> getJobsFromFile(std::string filename, int dataset_number)
     }
 
     return jobs;
+}
+
+
+std::vector<Job> getPDAlgorithmJobs(const std::vector<Job>& jobs)
+{
+    int N = jobs.size();
+
+    std::vector<Job> results;
+    std::vector<int> weighted_delays;
+    weighted_delays.push_back(0);
+
+    for (int i = 0; i < jobs.size(); i++)
+    {
+        auto jobs_copy = jobs;
+        auto job = jobs_copy[i];
+        jobs_copy.erase(jobs_copy.begin() + i);
+
+        auto cmax = getCmax(jobs_copy);
+
+        auto job_penalty = job.getPenalty(cmax);
+        auto K = job_penalty;
+
+        weighted_delays.push_back(getWeightedDelaysSum(jobs_copy));
+
+        auto F = job_penalty;
+
+        auto something = getWeightedDelaysSum(jobs_copy);
+    }
 }
